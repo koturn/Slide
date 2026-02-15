955e9e2558f038009cc85c4b89944368f346f316
koturn <jeak.koutan.apple@gmail.com>
2026-02-15 19:56:16 +0900

    chore: Add JSDoc comment

--- js/title.js
+++ js/title.js
@@ -1,8 +1,20 @@
 ;(function(global) {
   'use strict';
 
+  /**
+   * Cache of window.document.
+   * @type {Document}
+   */
   const doc = global.document;
+  /**
+   * Array of title tags, h1 ~ h6.
+   * @type {string[]}
+   */
   const titleTagNames = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+  /**
+   * Original document.title.
+   * @type {string}
+   */
   const originalTitle = doc.title;
 
   /**
