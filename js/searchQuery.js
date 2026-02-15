8cc7ad39db6ba9486a6fe486565ebcaf55164307
koturn <jeak.koutan.apple@gmail.com>
2026-02-15 19:54:56 +0900

    feat: use HEAD requests instead of GET requests for remote file existence checks

--- js/searchQuery.js
+++ js/searchQuery.js
@@ -6,30 +6,38 @@
    * @param {string} url - Target function.
    * @return Promise object of XMLHttpRequest.
    */
-  function sendGetRequest(url) {
-    const p = new Promise((resolve, reject) => {
+  const sendHeadRequest = typeof global.fetch !== 'undefined' ? function sendHeadRequest(url) {
+    return global.fetch(url, { method: 'HEAD' });
+  } : function sendHeadRequest(url) {
+    return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.addEventListener('load', e => resolve(xhr));
+      xhr.open('HEAD', url);
+      xhr.addEventListener('load', e => {
+        if (e.target.status === 200) {
+          resolve(xhr);
+        } else {
+          reject(xhr);
+        }
+      });
       xhr.send();
     });
-    return p;
   }
 
   /**
    * Send theme CSS.
    * @param {string} name - Theme name.
    */
-  async function setTheme(name) {
+  async function setThemeAsync(name) {
     const url = '../reveal.js/dist/theme/' + name + '.css';
     try {
       // Try to get content to know the content exists or not.
-      const xhr = await sendGetRequest(url);
-      if (xhr.status !== 404) {
+      const response = await sendHeadRequest(url);
+      if (response.status === 200) {
         document.getElementById('theme').setAttribute('href', url);
       }
     } catch (e) {
       console.error(e);
+      console.error('Theme css not found: ' + url);
     }
   }
 
@@ -37,31 +45,32 @@
    * Send syntax highlight CSS.
    * @param {string} name - Highlight name.
    */
-  async function setHighlight(name) {
+  async function setHighlightAsync(name) {
     const url = '../reveal.js/plugin/highlight/' + name + '.css';
     try {
       // Try to get content to know the content exists or not.
-      const xhr = await sendGetRequest(url);
-      if (xhr.status !== 404) {
+      const response = await sendHeadRequest(url);
+      if (response.status === 200) {
         document.getElementById('highlight').setAttribute('href', url);
       }
     } catch (e) {
       console.error(e);
+      console.error('Highlight css not found: ' + url);
     }
   }
 
-  global.setTheme = setTheme;
-  global.setHighlight = setHighlight;
+  global.setThemeAsync = setThemeAsync;
+  global.setHighlightAsync = setHighlightAsync;
 
   const searchParams = new URLSearchParams(global.location.search);
 
   const themeName = searchParams.get('theme');
   if (themeName !== null) {
-    setTheme(themeName);
+    setThemeAsync(themeName);
   }
 
   const highlightName = searchParams.get('highlight');
   if (highlightName !== null) {
-    setHighlight(highlightName);
+    setHighlightAsync(highlightName);
   }
 })((this || 0).self || global);
